e46b41c5707d1ce338a1e9711488b9774e52d83f
joaorobertoaquino <galvaojoaoroberto@gmail.com>
2024-11-01 13:33:58 -0300

    Mudanças nas interfaces do "Módulo Procedimento".

--- procedimento/procedimento.c
+++ procedimento/procedimento.c
@@ -8,18 +8,19 @@ void tela_procedimentos(void) {
   do {
     system("clear||cls");
     printf("\n");
-    printf("*******************************************************************************\n");
-    printf("***                                                                         ***\n");
-    printf("***                 ------ PROCEDIMENTOS ------                             ***\n");
-    printf("***                                                                         ***\n");
-    printf("***                 1. Cadastrar Procedimento                               ***\n");
-    printf("***                 2. Pesquisar Procedimento                               ***\n");
-    printf("***                 3. Atualizar Procedimento                               ***\n");
-    printf("***                 4. Remover Procedimento                                 ***\n");
-    printf("***                 0. Cancelar e sair                                      ***\n");
-    printf("***                                                                         ***\n");
-    printf("***                 Escolha a opção desejada: ");
-
+    printf("╔═════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                        ------ PROCEDIMENTOS ------                          ║\n");
+    printf("╠═════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║                                                                             ║\n");
+    printf("║                         1. Cadastrar Procedimento                           ║\n");
+    printf("║                         2. Pesquisar Procedimento                           ║\n");
+    printf("║                         3. Atualizar Procedimento                           ║\n");
+    printf("║                         4. Remover Procedimento                             ║\n");
+    printf("║                                                                             ║\n");
+    printf("║                         0. Cancelar e sair                                  ║\n");
+    printf("║                                                                             ║\n");
+    printf("╚═════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║  ↪Escolha a opção desejada: ");
     scanf("%d", &opcao);
     getchar();
     switch (opcao) {
@@ -46,16 +47,16 @@ void tela_procedimentos(void) {
 void tela_cadastrar_procedimento() {
   system("clear||cls");
   printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                         CADASTRAR PROCEDIMENTO                          ***\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***          ID Procedimento:                                               ***\n");
-  printf("***          Nome:                                                          ***\n");
-  printf("***          Duração:                                                       ***\n");
-  printf("***          CRE:                                                           ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
+  printf("╔═════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("║                         CADASTRAR PROCEDIMENTO                              ║\n");
+  printf("╠═════════════════════════════════════════════════════════════════════════════╣\n");
+  printf("║                                                                             ║\n");
+  printf("║    ID Procedimento:                                                         ║\n");
+  printf("║    Nome:                                                                    ║\n");
+  printf("║    Duração:                                                                 ║\n");
+  printf("║    CRE:                                                                     ║\n");
+  printf("║                                                                             ║\n");
+  printf("╚═════════════════════════════════════════════════════════════════════════════╝\n");
   printf("\n");
   printf("Pressione a tecla <ENTER> para continuar...\n");
   getchar();
@@ -63,13 +64,13 @@ void tela_cadastrar_procedimento() {
 void tela_atualizar_procedimento() {
   system("clear||cls");
   printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                         ATUALIZAR PROCEDIMENTO                          ***\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***       Informe o ID do procedimento que deseja atualizar:                ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
+  printf("╔═════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("║                         ATUALIZAR PROCEDIMENTO                              ║\n");
+  printf("╠═════════════════════════════════════════════════════════════════════════════╣\n");
+  printf("║                                                                             ║\n");
+  printf("║    Informe o ID do procedimento que deseja atualizar:                       ║\n");
+  printf("║                                                                             ║\n");
+  printf("╚═════════════════════════════════════════════════════════════════════════════╝\n");
   printf("\n");
   printf("Pressione a tecla <ENTER> para continuar...\n");
   getchar();
@@ -77,13 +78,13 @@ void tela_atualizar_procedimento() {
 void tela_deletar_procedimento() {
   system("clear||cls");
   printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                           DELETAR PROCEDIMENTO                          ***\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***       Informe o ID do procedimento que deseja deletar:                  ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
+  printf("╔═════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("║                            DELETAR PROCEDIMENTO                             ║\n");
+  printf("╠═════════════════════════════════════════════════════════════════════════════╣\n");
+  printf("║                                                                             ║\n");
+  printf("║    Informe o ID do procedimento que deseja deletar:                         ║\n");
+  printf("║                                                                             ║\n");
+  printf("╚═════════════════════════════════════════════════════════════════════════════╝\n");
   printf("\n");
   printf("Pressione a tecla <ENTER> para continuar...\n");
   getchar();
@@ -91,13 +92,13 @@ void tela_deletar_procedimento() {
 void tela_ver_procedimento() {
   system("clear||cls");
   printf("\n");
-  printf("*******************************************************************************\n");
-  printf("***                              VER PROCEDIMENTO                           ***\n");
-  printf("*******************************************************************************\n");
-  printf("***                                                                         ***\n");
-  printf("***       Informe o ID do procedimento que deseja ver informações:          ***\n");
-  printf("***                                                                         ***\n");
-  printf("*******************************************************************************\n");
+  printf("╔═════════════════════════════════════════════════════════════════════════════╗\n");
+  printf("║                              VER PROCEDIMENTO                               ║\n");
+  printf("╠═════════════════════════════════════════════════════════════════════════════╣\n");
+  printf("║                                                                             ║\n");
+  printf("║    Informe o ID do procedimento que deseja ver informações:                 ║\n");
+  printf("║                                                                             ║\n");
+  printf("╚═════════════════════════════════════════════════════════════════════════════╝\n");
   printf("\n");
   printf("Pressione a tecla <ENTER> para continuar...\n");
   getchar();
